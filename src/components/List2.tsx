f9c57771cd84853bab8727eeabc525f2f8f6bb26
Matt Nickerson <mattwnickerson39@gmail.com>
2025-04-04 01:19:13 -0400

    Matt Stuff

---
+++ src/components/List2.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const List2: React.FC = () => {
+    return (
+        <div>
+            <h4>Some parts of a Snowmobile:</h4>
+            <ul className="parts">
+                <li style={{ color: 'darkcyan' }}>Body</li>
+                <li>Skis</li>
+                <li>Track</li>
+                <li>Snow Flap</li>
+                <li>Skid</li>
+            </ul>
+        </div>
+    );
+};
+
+export default List2;
